877cfbe9a637cb023f71a01d0adeca21e1889baf
Stefano Harding <riddopic@gmail.com>
2016-10-02 12:48:27 -0700

    update jre

--- versions/8/config.mk
+++ versions/8/config.mk
@@ -4,5 +4,5 @@ export JRE_VERSION = 8
 export JRE_UPDATE = 101
 export JRE_BUILD = 13
 export JRE_PACKAGE = jre
-export JRE_SHA256 = 467f323ba38df2b87311a7818bcbf60fe0feb2139c455dfa0e08ba7ed8581328
+export JRE_SHA256 = 33026a91f4acb751979f05a5c72b65feaabd5b5c18604e62bc94d2feea7f7523
 export JRE_HOME = /opt/jdk
